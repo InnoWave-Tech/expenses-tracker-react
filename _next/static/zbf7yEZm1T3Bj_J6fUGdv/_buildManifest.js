3960634a35f1a7931004c552ec63089b845f6c17
UltimateForm <heliojmsemedo@gmail.com>
2021-12-09 16:36:17 +0000

    Deploying to ghpages from @ InnoWave-Tech/expenses-tracker-react@41a160ba8b9bc240b4f0a523c967a112590267e7 🚀

---
+++ _next/static/zbf7yEZm1T3Bj_J6fUGdv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7815ee65fcde6e19.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/expenses":["static/chunks/pages/expenses-1d0404e2f1b8ba67.js"],sortedPages:["/","/_app","/_error","/expenses"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
