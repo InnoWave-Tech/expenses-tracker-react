5bf28adc0c16bed16c071fc67add9a293581ba1a
UltimateForm <heliojmsemedo@gmail.com>
2021-12-09 15:53:22 +0000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    assetPrefix: process.NODE_ENV==="production" ? "/expenses-tracker-react/" : "/",
+    basePath: process.NODE_ENV==="production" ? "/expenses-tracker-react/" : "/, 
+}
